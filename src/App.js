537b900e416830206ea62f9bcb7a34ed5dedc83d
Devin Dias <devindias.tech@gmail.com>
2020-01-21 05:59:43 -0600

    built scoreboard component
    built form
    built lower scoreboard
    built team score components
    styles the board
    broke everything into seperate components

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Form from './components/Form'
+
+
+function App() {
+  
+ return(
+    
+    <Form />
+
+ );
+}
+
+export default App;
---
+++ src/components/TopRow.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function TopRow(props){
+    return (
+
+<div className="topRow"> 
+    <div className="home">
+        <h2 className="home__name">{props.input.homeTeam}</h2>
+        <div className="home__score">{props.homeScore}</div>
+        </div>
+    <div className="timer">15:00</div>
+    <div className="away">
+        <h2 className="away__name">{props.input.awayTeam}</h2>
+        <div className="away__score">{props.awayScore}</div>
+    </div>
+</div>
+
+    )
+//comment
+}
